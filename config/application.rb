ef16334d9ff875c697884746b2a45ca09c4f6a84
« cbograt » <christopher.bograt@gmail.com>
2019-12-02 15:07:25 +0100

    Fixed typo

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module ScoplyBack
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.middleware.insert_before 0, 'Rack::Cors' do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource '*',
