1247b4cc91b9413012222b35c735133f843262ad
« cbograt » <christopher.bograt@gmail.com>
2019-12-02 15:01:14 +0100

    Configured headers

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,15 @@ module ScoplyBack
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.middleware.insert_before 0, 'Rack::Cors' do
+      allow do
+        origins '*'
+        resource '*',
+          headers:  :any,
+          expose:   ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+          methods:  %i[get post options delete put]
+      end
+    end
   end
 end
